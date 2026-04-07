d272782683a28e0f937bac5456b37d9f282aba3a
mastergitter69 <schmidtmarcus00@gmail.com>
2026-04-07 08:56:22 +0200

    Magnet Enchant angefangen

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -27,6 +27,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+import com.example.examplemod.ModEnchantments;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
@@ -60,9 +61,11 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public ExampleMod();
     {
         IEventBus modEventBus = context.getModEventBus();
+
+        ModEnchantments.ENCHANTMENTS.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
---
+++ src/main/java/com/example/examplemod/ModEnchantments.java
@@ -0,0 +1,17 @@
+package com.example.examplemod;
+
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModEnchantments {
+
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS =
+            DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, "examplemod");
+
+    public static final RegistryObject<Enchantment> MAGNET =
+            ENCHANTMENTS.register("magnet", MagnetEnchantment::new);
+
+}
+
