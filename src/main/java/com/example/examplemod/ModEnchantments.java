a8bb908fdbdaf85cecfe1c1cafcf2c09e36744bc
mastergitter69 <schmidtmarcus00@gmail.com>
2026-04-21 09:10:15 +0200

    MagnetEnchant in EnchantTable

--- src/main/java/com/example/examplemod/ModEnchantments.java
+++ src/main/java/com/example/examplemod/ModEnchantments.java
@@ -4,14 +4,14 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
+//Registiert alle Enchantments der Mod
 public class ModEnchantments {
 
     public static final DeferredRegister<Enchantment> ENCHANTMENTS =
             DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, "examplemod");
-
+//DefferedRegister speichert alle Enchantments und registiert sie später beim Laden der Mod
     public static final RegistryObject<Enchantment> MAGNET =
             ENCHANTMENTS.register("magnet", MagnetEnchant::new);
-
+//Registiert das Magnet Enchant unter dem namen magnet
 }
 
