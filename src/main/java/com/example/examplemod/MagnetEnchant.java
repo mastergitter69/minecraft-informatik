675e9d8bd7069733643f12b88d23d01cd363e624
mastergitter69 <schmidtmarcus00@gmail.com>
2026-04-21 08:47:19 +0200

    MagnetEnchant in EnchantTable

--- src/main/java/com/example/examplemod/MagnetEnchant.java
+++ src/main/java/com/example/examplemod/MagnetEnchant.java
@@ -4,18 +4,18 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
-
+// Magnet Enchant: Zieht Getroffene Mobs oder Spieler nach einem Treffer zum Spieler hin.
 public class MagnetEnchant extends Enchantment {
     public MagnetEnchant(){
         super(Rarity.COMMON, EnchantmentCategory.WEAPON,
                 new EquipmentSlot[]{EquipmentSlot.MAINHAND});
     }
-
+// Enchant wird als COMMON eingestuft und kann nur auf Waffen in der Haupthand verwendet werden.
     @Override
     public int getMaxLevel(){
         return 3; // Stufen I, II, III
     }
-
+// Maximale Stufe des Enchantments ist 3
     @Override
     public void doPostAttack(LivingEntity attacker,
                              net.minecraft.world.entity.Entity target,
@@ -35,3 +35,11 @@ public class MagnetEnchant extends Enchantment {
         }
     }
 }
+/**
+ * Wird nach jedem Angriff ausgeführt
+ * Berechnet die Richtung vom Ziel zum Angreifer und zieht das Ziel heran
+ * Wird nur auf dem Server oder lokalem Server ausgeführt und nicht auf dem Client
+ * Berechnet den Abstand zwischen Angreifer und Ziel auf allen 3 Achsen
+ * Je höher das Level desto stärker wird das Ziel herangezogen
+ * Setzt die Bewegung des Ziels in Richtung des Angreifers
+ */
